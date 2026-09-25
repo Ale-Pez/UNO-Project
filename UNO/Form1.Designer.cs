433a104c0a1d2138d30e8f2e6add749cdbea1ed5
Iker Alejandro Arriaga López <ikerlopez1809@gmail.com>
2026-09-25 15:25:06 -0600

    Plantilla de la interfaz

--- UNO/Form1.Designer.cs
+++ UNO/Form1.Designer.cs
@@ -28,13 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pila_cartas = new System.Windows.Forms.PictureBox();
+            this.mazo_cartas = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cartas_jugador1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.cartas_jugador2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.cartas_jugador3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pila_cartas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mazo_cartas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 350);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.cartas_jugador1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Jugador 1";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 350);
+            this.panel2.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Controls.Add(this.cartas_jugador3);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 350);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Jugador 3";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.groupBox2);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(600, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 350);
+            this.panel4.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.cartas_jugador2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 350);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Jugador 2";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.groupBox4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(200, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(400, 350);
+            this.panel3.TabIndex = 4;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.pila_cartas);
+            this.groupBox4.Controls.Add(this.mazo_cartas);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(0, 0);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(400, 350);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Mesa";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mazo";
+            // 
+            // pila_cartas
+            // 
+            this.pila_cartas.BackgroundImage = global::UNO.Properties.Resources.back_uno;
+            this.pila_cartas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pila_cartas.Location = new System.Drawing.Point(161, 127);
+            this.pila_cartas.Name = "pila_cartas";
+            this.pila_cartas.Size = new System.Drawing.Size(68, 103);
+            this.pila_cartas.TabIndex = 1;
+            this.pila_cartas.TabStop = false;
+            // 
+            // mazo_cartas
+            // 
+            this.mazo_cartas.BackgroundImage = global::UNO.Properties.Resources.back_uno;
+            this.mazo_cartas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.mazo_cartas.Location = new System.Drawing.Point(26, 34);
+            this.mazo_cartas.Name = "mazo_cartas";
+            this.mazo_cartas.Size = new System.Drawing.Size(68, 103);
+            this.mazo_cartas.TabIndex = 0;
+            this.mazo_cartas.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pila de cartas";
+            // 
+            // cartas_jugador1
+            // 
+            this.cartas_jugador1.Location = new System.Drawing.Point(200, 19);
+            this.cartas_jugador1.Name = "cartas_jugador1";
+            this.cartas_jugador1.Size = new System.Drawing.Size(400, 69);
+            this.cartas_jugador1.TabIndex = 0;
+            // 
+            // cartas_jugador2
+            // 
+            this.cartas_jugador2.Location = new System.Drawing.Point(53, 34);
+            this.cartas_jugador2.Name = "cartas_jugador2";
+            this.cartas_jugador2.Size = new System.Drawing.Size(123, 316);
+            this.cartas_jugador2.TabIndex = 0;
+            // 
+            // cartas_jugador3
+            // 
+            this.cartas_jugador3.Location = new System.Drawing.Point(22, 34);
+            this.cartas_jugador3.Name = "cartas_jugador3";
+            this.cartas_jugador3.Size = new System.Drawing.Size(123, 316);
+            this.cartas_jugador3.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::UNO.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.Text = "UNO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pila_cartas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mazo_cartas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.PictureBox mazo_cartas;
+        private System.Windows.Forms.PictureBox pila_cartas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel cartas_jugador1;
+        private System.Windows.Forms.FlowLayoutPanel cartas_jugador2;
+        private System.Windows.Forms.FlowLayoutPanel cartas_jugador3;
     }
 }
 
--- UNO/Form1.cs
+++ UNO/Form1.cs
@@ -16,5 +16,9 @@ namespace UNO
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
     }
 }
